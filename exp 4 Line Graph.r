799ae61f4cfe3a858931d11f002ecabb89c9a54f
kamini8888 <112712900+kamini8888@users.noreply.github.com>
2024-02-02 20:49:21 +0530

    Create exp 4 Line Graph.r

---
+++ exp 4 Line Graph.r
@@ -0,0 +1,22 @@
+# Provided data
+sales_data <- data.frame(
+  Month = c('January', 'February', 'March', 'April', 'May'),
+  Sales = c(15000, 18000, 22000, 20000, 23000)
+)
+
+# Convert Month to factor
+sales_data$Month <- factor(sales_data$Month, levels = unique(sales_data$Month))
+
+# Scatter Plot
+plot(sales_data$Month, sales_data$Sales, col = "blue", pch = 16,
+     main = "Scatter Plot - Time Series Analysis", xlab = "Month", ylab = "Sales (in $)",
+     ylim = c(0, max(sales_data$Sales) + 5000))
+
+# Line Graph
+lines(sales_data$Month, sales_data$Sales, type = "o", col = "green", lwd = 2)
+
+# Add grid for better readability
+grid()
+
+# Add legend
+legend("topright", legend = c("Sales"), col = c("green"), lty = 1, lwd = 2, bg = "white")
